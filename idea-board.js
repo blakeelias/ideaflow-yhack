8d2a60357d339196a3aac192be6df4ad83feffc2
Jacob Cole <tmad4000@gmail.com>
2014-11-02 04:48:05 -0800

    fix imports

--- idea-board.js
+++ idea-board.js
@@ -390,12 +390,82 @@ Router.route('/idea/:_id(*)', function () {
       Session.set("search_input", $('input[name=search]').val());
     },
 
-    'a.breadcrumb click': function(){
+    // 'a.breadcrumb': function(){
+    //   // history.pushState({}, '', $(event.target).attr("href"));
+    //   // return false;
+    // },
+    'click .import-link': function(){
+      Session.set("importShow",!Session.get("importShow"))
+      return false;
       // history.pushState({}, '', $(event.target).attr("href"));
       // return false;
-    }
-    'a#import-link click': function(){
-      Session.set("importShow",!Session.get("importShow"))
+    },
+
+    'click .import-submit': function(e,t){
+      var nodes = false;
+      var edges = false;
+      try {
+        nodes = $.parseJSON($('.import-nodes').val().trim());
+        edges = $.parseJSON($('.import-edges').val().trim());
+      }      
+      catch (err) {
+        // Do something about the exception here
+      }
+      var textParam=$(".import-text-param").val() || "text"; 
+      var detailParam=$(".import-detail-param").val() || false; 
+
+
+      var msg = '';
+      var nodeCount = 0;
+      iray = [];
+      if (nodes) {
+         nodes.forEach(function(idea) {
+                if (idea._id) 
+                  delete idea._id;
+
+                idea["text"]=idea[textParam] || "";
+
+                if (detailParam && idea[detailParam]) {
+                  idea["text"]+="\n\n -- " + idea[detailParam];
+                  console.log(idea[detailParam])
+                }
+
+                idea["searchCache"] = idea["text"];
+                idea["relations"] = {};
+                idea["parent_id"] = Session.get("current_idea")._id;
+                idea["date_created"] = new Date().getTime();
+                idea["status"] = 0;
+
+
+                iray.push(insertIdea(idea));
+                nodeCount++;
+            });
+
+         msg += (nodeCount) + ' nodes successfully imported. '
+      }
+      else {
+        msg += 'No nodes imported. ';
+      }
+
+      var edgeCount = 0;
+      if (edges) {
+         edges.forEach(function(edge) {
+                insertRelationBi(iray[edge.source], iray[edge.target])
+              
+                edgeCount++;
+            });
+
+         msg += (edgeCount) + ' edges successfully imported. '
+      }
+      else {
+        msg += 'No edges imported. ';
+      }
+
+     
+
+
+      console.log(msg)
+      return false;
       // history.pushState({}, '', $(event.target).attr("href"));
       // return false;
     }
@@ -413,8 +483,6 @@ Router.route('/idea/:_id(*)', function () {
     var svg, 
         width = 900, 
         height = 900;
-
-    var mousedown_node;
 
     function wrap(text, width) {
       text.each(function() {
@@ -454,62 +522,6 @@ Router.route('/idea/:_id(*)', function () {
       .attr("class", "overlay")
       .attr("width", width)
       .attr("height", height);
-
-    // line displayed when dragging new nodes
-    var drag_line = svg.append('svg:path')
-      .attr('class', 'link dragline hidden')
-      .attr('d', 'M0,0L0,0');
-
-    var force = d3.layout.force();
-
-    // define arrow markers for graph links
-    svg.append('svg:defs').append('svg:marker')
-        .attr('id', 'end-arrow')
-        .attr('viewBox', '0 -5 10 10')
-        .attr('refX', 6)
-        .attr('markerWidth', 3)
-        .attr('markerHeight', 3)
-        .attr('orient', 'auto')
-      .append('svg:path')
-        .attr('d', 'M0,-5L10,0L0,5')
-        .attr('fill', '#000');
-
-    svg.append('svg:defs').append('svg:marker')
-        .attr('id', 'start-arrow')
-        .attr('viewBox', '0 -5 10 10')
-        .attr('refX', 4)
-        .attr('markerWidth', 3)
-        .attr('markerHeight', 3)
-        .attr('orient', 'auto')
-      .append('svg:path')
-        .attr('d', 'M10,-5L0,0L10,5')
-        .attr('fill', '#000');
-
-
-    svg
-      .on('mousemove', function(d){  
-        // update drag line
-        if(mousedown_node) {
-          drag_line.attr('d', 'M' + mousedown_node.x + ',' + mousedown_node.y + 'L' + d3.mouse(this)[0] + ',' + d3.mouse(this)[1]);
-        }
-        
-      })
-      .on('mouseup', function(d){
-        if(mousedown_node) {
-          // hide drag line
-          drag_line
-            .classed('hidden', true)
-            .style('marker-end', '');
-          force.resume();
-
-          mousedown_node = null;
-        }
-      })
-
-
-    var fill = d3.scale.category10();
-    // console.log(ideas)
-
     Tracker.autorun(function () {
       var id = Session.get("current_idea");
       if(!id) return;
@@ -517,7 +529,7 @@ Router.route('/idea/:_id(*)', function () {
       console.log(id, 'current idea id')
       var ideas = Ideas.find(
         { parent_id: id._id }, 
-        {sort: {date_created: 1}}
+        {sort: {date_created: -1}}
       ).fetch();
 
       // #future for related ideas outside current scope
@@ -546,6 +558,8 @@ Router.route('/idea/:_id(*)', function () {
         })
       }));
 
+      var fill = d3.scale.category10();
+      // console.log(ideas)
 
       function tick(e) {
         node.attr("cx", function(d) { return d.x; })
@@ -565,79 +579,68 @@ Router.route('/idea/:_id(*)', function () {
 
       }
 
-      force
+      var force = d3.layout.force()
         .distance(80)
+        // .charge(function(link){
+        //   console.log(link)
+        //   var ax0 = link.source.x,
+        //       ax1 = link.source.x + link.source.width,
+        //       ay0 = link.source.y,
+        //       ay1 = link.source.y + link.source.height,
+        //       bx0 = link.target.x,
+        //       bx1 = link.target.x + link.target.width,
+        //       by0 = link.target.y,
+        //       by1 = link.target.y + link.target.height;
+              
+        //   var width = Math.min(ax1, bx1) - Math.max(ax0, bx0),
+        //       height = Math.min(ay1, by1) - Math.max(ay0, by0);
+
+        //   return -Math.sqrt(width * width + height * height);
+        // })
         .charge(-400)
         .nodes(data)
         .links(links)
         .size([width, height])
         .on("tick", tick)
-        // .resume()
-        .start()
+        
+        .start();
 
-      
+
+      var onclick = function(d){
+        console.log('onclick')
+        // d3.select(this).classed("fixed", d.fixed = true);
+        Session.set("selected_idea", d)
+      }
+      var drag = force.drag()
+        // .on('dragstart', function(d){
+        //   d3.select(this).classed("fixed", d.fixed = false);
+        // });
+
+        // .on('dragend', function(d){
+        //   d3.select(this).classed("fixed", d.fixed = true);
+        // })
       var link = svg.selectAll(".link")
-        .data(links);
-      
-      link.exit().remove();
-
-      link
+        .data(links)
         .enter().append("line")
           .attr("class", "link");
-      
 
       var node = svg.selectAll(".node")
-        .data(data);
-
-      node.exit().remove();
-
-      node.enter().append("circle")
+        .data(data)
+      .enter().append("circle")
         .attr("class", "node")
         .attr("x", function(d) { return d.x; })
         .attr("y", function(d) { return d.y; })
         .attr("width", function(d){ return d.width })
         .attr("height", function(d){ return d.height })
         .attr("r", 8)
-        .style("fill", function(d, i) { return fill(i & 3); })
-        .style("stroke", function(d, i) { return d3.rgb(fill(i & 3)).darker(2); })
-        // .call(drag)
-        .on('click', function(d){
-          // console.log('onclick')
-          // d3.select(this).classed("fixed", d.fixed = true);
-          Session.set("selected_idea", d)
-        })
-        .on('mousedown', function(d){
-          d3.event.stopPropagation()
-          // console.log('mousing down')
-          // reposition drag line
-          mousedown_node = d;
-          force.stop()
-
-          drag_line
-            .style('marker-end', 'url(#end-arrow)')
-            .classed('hidden', false)
-            .attr('d', 'M' + mousedown_node.x + ',' + mousedown_node.y + 'L' + mousedown_node.x + ',' + mousedown_node.y);
-        })
-        .on('mouseup', function(d){
-          console.log('test test test test')
-          if(mousedown_node && mousedown_node != d){
-            // links.push({source: mousedown_node, target: d})
-            // console.log('mousing up', mousedown_node, d) 
-            var sourceId = mousedown_node.idea._id;
-            var targetId = d.idea._id;
-
-            var newRelation ={}
-            newRelation["relations."+targetId]={weight:1,reviewed:true}
-
-            Ideas.update({_id:sourceId},{$set: newRelation})
-            var newRelationBack ={}
-            newRelationBack["relations."+sourceId]={weight:1,reviewed:true}
-            Ideas.update({_id:targetId},{$set: newRelationBack})     
-
-          }
-        })
+        .style("fill", function(d, i) {  return d.idea.color || "#CCC"; })
+        .style("stroke", function(d, i) { return d3.rgb(fill(3)).darker(2); })
+        // .style("fill", function(d, i) { return fill(i & 3); })
+        // .style("stroke", function(d, i) { return d3.rgb(fill(i & 3)).darker(2); })
+        .call(drag)
+        .on('click', onclick)
         .on('mouseover', function(d){
-          // console.log('mouse over', d)
+          console.log('mouse over', d)
           // var nodeSelection = d3.select(this).style({opacity:'0.8'});
           // nodeSelection.select("text").style({opacity:'1.0'});
           Session.set("selected_idea", d)
@@ -649,11 +652,9 @@ Router.route('/idea/:_id(*)', function () {
           // Session.set("selected_idea", null);
         })
         
-      var labels = svg.selectAll(".nodelabels")
-        .data(data);
-
-      labels.enter().append("text")
-        .attr("class", "nodelabels")
+      var labels = svg.selectAll(".labels")
+        .data(data)
+      .enter().append("text")
         .attr("x", function(d) { return 0; })
         .attr("y", function(d) { return 0; })
         // .attr("text-anchor", "middle")
@@ -665,7 +666,7 @@ Router.route('/idea/:_id(*)', function () {
         })
         .call(wrap, 100)
 
-      labels.exit().remove();
+    
 
       force.resume()
 
